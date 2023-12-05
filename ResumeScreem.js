3d9f1a13676601c5b9de5b50cbfab723b2f9e19a
NatanGonk <127403597+NatanGonk@users.noreply.github.com>
2023-12-05 18:00:24 -0300

    ResumeScreem.js

---
+++ ResumeScreem.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ResumeScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Meu Currículo</Text>
+      {/*Natan Gonçalves de Barros Melo 
+22 anos , Solteiro 
+
+Dados pessoais 
+
+E-mail: goncalvesnatan08@gmail.com 
+País de Nacionalidade: Brasil 
+Telefone: 985695964 
+Celular: (81)986724789 
+Endereço: Rua Severino Monteiro de Jesus Janga 53437440 Paulista Pernambuco-Brasil 
+
+Objetivos 
+
+Ingressar no mercado de trabalho, obtendo a oportunidade de desenvolver-me profissionalmente e fazer a diferença 
+na empresa. 
+
+Resumo Profissional 
+
+Ainda não possuo experiência profissional. 
+Habilidades: organização; facilidade e empenho em trabalhar em equipe; agilidade na hora de serviço; pontualidade. 
+Qualificações: Paciência; vontade de mostrar serviço. 
+Pacote Office Completo (Word, Excel, Power Point) 
+
+Formação 
+
+Escolaridade 
+Ensino Médio (2o. Grau) completo 
+
+Graduação 
+Análise e desenvolvimento de sistemas SENAC (Cursando - 3° período) 
+Cursos Complementares 
+Coletivo Online, Instituto Coca Cola Brasil 
+(Setembro de 2021) - Concluído 
+
+Pretensão salarial 
+
+Negociável 
+
+Região de interesse 
+Preferência pela região de Recife/Pernambuco/, ou num raio de 50 km. Aceita considerar propostas de outras regiões. 
+Informações complementares
+Ainda não tenho experiência profissional ou informal, tenho interesse em adquirir com o tempo experiência e 
+conhecimento nas áreas que almejo para que eu me torne um profissional capacitado no futuro.
+*/}
+    </View>
+  );
+};
+
+export default ResumeScreen;
